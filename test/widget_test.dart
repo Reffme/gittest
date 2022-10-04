069779e8937e2dd89d79fd4a9b7b6ea1804d8348
Reffme <sidorov.inc.one@gmail.com>
2022-10-04 21:55:05 +0700

    two commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,11 +13,10 @@ import 'package:flutter_application_bool/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    // РУРУРУРУРУРУР
+    //sdgsdghds
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
